23e5be1339d0638d6d9605ba3656afe6a9178e21
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-08-08 21:35:29 -0400

    agregando titulo y NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+//components!
+import './components/NavBar';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
+
+
+class App extends Component
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <h1>ElchickenPe</h1>
+    )
+  }
+
 function App() {
   return (
     <div className="App">
